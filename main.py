70dabf134d26d715d2dfcddc3d1f8272337deece
Edilsondala27 <147382256+Edilsondala27@users.noreply.github.com>
2023-10-10 20:34:29 +0100

    Update main.py
    Add kivymd

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-import kivy
-# kivy.require('2.1.0') replace with your current kivy version !
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+from kivy.lang import Builder
 
-from kivy.app import App
-from kivy.uix.label import Label
+
+KV = """
+<ScreenOne>
+    name: 's1'
+    MDRaisedButton:
+        text: 'panzo'
+        pos_hint:{'center_x':.5 , 'center_y':.5}
+        on_release: 
+            root.manager.transition.direction = 'left'
+            root.manager.current = 's2'
+            print("amor")
+
+<ScreenTwo>
+    name: 's2'
+    MDRaisedButton:
+        text: 'dala'
+        pos_hint:{'center_x':.5 , 'center_y':.5}
+        on_release: 
+            root.manager.transition.direction = 'right'
+            root.manager.current = 's1'
+"""
+
+class ScreenOne(MDScreen):
+    pass
+
+class ScreenTwo(MDScreen):
+    pass
 
 
-class MyApp(App):
+
+class TestApp(MDApp):
 
     def build(self):
-        return Label(text='world')
-
+        Builder.load_string(KV)
+        sm = MDScreenManager()
+        sm.add_widget(ScreenOne())
+        sm.add_widget(ScreenTwo())
+    
+        return sm
 
 if __name__ == '__main__':
-    MyApp().run()
+    TestApp().run()
